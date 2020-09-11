f77d2b0160a5656020b8a9fcf978efcfa8b050d5
KiwiMantelli <marina.13@free>
2020-09-11 16:07:46 +0200

    number of question update

--- src/index.js
+++ src/index.js
@@ -144,7 +144,7 @@ function clearStatus() {
 
 function endGame() {
   console.log(tenQuestion);
-  if (tenQuestion < 6) {
+  if (tenQuestion < 9) {
     console.log("test end Game");
     tenQuestion++;
   } else {
